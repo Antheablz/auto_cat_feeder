dc2e080f9b6f05783259ab9c4466dccd9dd22793
Antheablz <agblais@icloud.com>
2026-02-20 14:10:17 -0800

    confirmed that wifi is working

--- main/main.c
+++ main/main.c
@@ -14,13 +14,25 @@
 
 void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
+        // ESP_ERROR_CHECK(esp_wifi_connect());
+        if (esp_wifi_connect() == ESP_OK) {
+            printf("Connected to wifi\n");
+        }
+    }
+}
+
+void ip_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+
+        char *ip = ip4addr_ntoa(&event->ip_info.ip);
+        printf("Got ip: %s\n", ip);
     }
 }
 
 void connect_to_wifi(){
     // for grabbing IP info
-    // tcpip_adapter_init();
+    tcpip_adapter_init();
 
     // ESP_ERROR_CHECK is similar to assert. If arg of ESP_ERR_CHECK != ESP_ok, then err msg is printed and abort() is called
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -29,6 +41,7 @@ void connect_to_wifi(){
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL));
 
     wifi_config_t wifi_cfg = {
         .sta = {
@@ -41,7 +54,9 @@ void connect_to_wifi(){
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_cfg));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    printf("connected to wifi i think\n");
+    // need to wait and confirm that esp is actually connected to wifi before unregistering events
+    // ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler));
+    // ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler));
 }
 
 

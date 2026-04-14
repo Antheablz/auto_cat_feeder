b0954b33da32e7e1ab08a678914ee20ef0b855ed
Antheablz <agblais@icloud.com>
2026-04-14 15:50:01 -0700

    setting up the webserver endpoint handlers

--- firmware/main/server.c
+++ firmware/main/server.c
@@ -11,6 +11,18 @@
 
 #include "server.h"
 
+esp_err_t webpage_files_handler(httpd_req_t *req)
+{
+    // Handle the request
+    // ...
+
+    // Return ESP_OK if the request was handled successfully
+    return ESP_OK;
+
+    // Return an error code to close the connection
+    // return ESP_FAIL;
+}
+
 void start_rest_server() {
     
     // Generate default configuration
@@ -22,8 +34,24 @@ void start_rest_server() {
 
     // ESP_ERROR_CHECK(httpd_start(&server, &config));
 
-    if (httpd_start(&server, &config) == ESP_OK) {
-        printf("-----> Server Started??\n");
-        //now we register uri handlers
+    if (httpd_start(&server, &config) != ESP_OK) {
+        printf("-----> Server failed to start!\n");
+        //free rest_context var
+        //return an error. eventually set xEventGroupWaitBits thing or event off?
+        //look into the built in esp_http_server_event_id_t for event handling??
     }
+
+    printf("-----> Server Started\n");
+
+    //now we register uri handlers
+    /* URI handler for getting web server files */
+    httpd_uri_t webpage_files_uri= {
+        .uri        = "/*",
+        .method     = HTTP_GET,
+        .handler    = webpage_files_handler,
+        .user_ctx   = NULL
+    };
+    httpd_register_uri_handler(server, &webpage_files_uri);
+
+    /* URI handler for turning LED on and off */
 }
--- firmware/main/server.h
+++ firmware/main/server.h
@@ -1,6 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include "esp_http_server.h"
+
+esp_err_t webpage_files_handler(httpd_req_t *req);
 void start_rest_server(void);
 
 #endif /* SERVER_H */

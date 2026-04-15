c6ba1fb273799fbf99087f214378c1d9d0205c8b
Antheablz <agblais@icloud.com>
2026-04-14 18:01:47 -0700

    updating constants

--- firmware/main/constants.h
+++ firmware/main/constants.h
@@ -1,11 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define WIFI_USER "SHAW-ACDC2.4"
-#define WIFI_PASS "bagel9946candid"
+#define WIFI_USER "USER"
+#define WIFI_PASS "PASS"
 
-// #define WIFI_USER "TELUS0901"
-// #define WIFI_PASS "hmchDZApCT3s"
+
 
 
 #endif

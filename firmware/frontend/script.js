c787bff45e6666d4252a0585c3e86d20d013fb23
Antheablz <agblais@icloud.com>
2026-05-12 16:40:10 -0700

    removing old comments

--- firmware/frontend/script.js
+++ firmware/frontend/script.js
@@ -1,14 +1,3 @@
-// fetch('http://esp32s2.local/blink', {
-//   method: 'PUT',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify({
-//     state: 1
-//   }),
-// })
-// .then(response => response)
-// // .then(response => response.json())
-// .then(data => console.log('Success:', data))
-// .catch(error => console.error('Error:', error));
 
 function toggle_led_put(val) {
   console.log("toggle led", val);
@@ -26,21 +15,3 @@ function toggle_led_put(val) {
   .then(data => console.log('Success:', data))
   .catch(error => console.error('Error:', error));
 }
-
-
-
-
-
-
-
-// Simple PUT request with a JSON body using fetch
-// const element = document.querySelector('#put-request .date-updated');
-// const element = document.getElementById('button-on');
-// const requestOptions = {
-//     method: 'PUT',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ title: 'Fetch PUT Request Example' })
-// };
-// fetch('http://esp32s2.local/blink', )
-//     .then(response => response.json())
-//     .then(data => element.innerHTML = data );
